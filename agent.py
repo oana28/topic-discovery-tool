006bf00ffdc7172e2780cafc170b71a83ee11067
oana28 <257713081+oana28@users.noreply.github.com>
2026-02-21 22:45:48 +0200

    Make changes to the structure of the agent code

--- agent.py
+++ agent.py
@@ -15,21 +15,30 @@ def search_info_on_web(query:str)->Dict[str, Any]:
     """Search information on the web"""
     return tavily.search(query)
 
-prompt = """You are an ai assistant with access to web search and up-to-date data retrieval. The user will give you a questions about a domain they care about(tech, news, sports, etc.). 
-Using the search info on web tool, search the web for the latest information relevant to their question. Return a clear, accurate answer to the user and offer more information or details if requested."""
+def agent_creation():
+    prompt = """You are an ai assistant with access to web search and up-to-date data retrieval. The user will give you a questions about a domain they care about(tech, news, sports, etc.). 
+    Using the search info on web tool, search the web for the latest information relevant to their question. Return a clear, accurate answer to the user and offer more information or details if requested."""
 
-agent = create_agent(
-    model=ChatOllama(model="qwen3"),
-    tools=[search_info_on_web],
-    system_prompt=prompt,
-    checkpointer=InMemorySaver()
-)
+    agent = create_agent(
+        model=ChatOllama(model="qwen3"),
+        tools=[search_info_on_web],
+        system_prompt=prompt,
+        checkpointer=InMemorySaver()
+    )
 
-config = {"configurable": {"thread_id": "1"}}
+    return agent
 
-while True:
-    uinput = input("Write a question (or exit for quit): ")
-    if uinput.lower() == "exit":
-        break
-    response = agent.invoke({"messages":[uinput]},config)
-    print(response['messages'][-1].content)
+def get_config(thread_id:str):
+    return {"configurable": {"thread_id":thread_id}}
+
+def main():
+    agent = agent_creation()
+    config = get_config("1")
+    while True:
+        uinput = input("Write a question (or exit for quit): ")
+        if uinput.lower() == "exit":
+            break
+        response = agent.invoke({"messages":[uinput]},config)
+        print(response['messages'][-1].content)
+
+main()
